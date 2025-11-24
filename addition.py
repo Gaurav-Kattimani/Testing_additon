7c9668106eb7185c9d1c0ec80041f06d53cd12fd
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-24 16:43:29 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -3,12 +3,19 @@ import sys
 def add(a, b):
     return a + b
 
-if _name_ == "_main_":
+def main():
     if len(sys.argv) != 3:
-        print("Usage: python add.py <num1> <num2>")
+        print("Usage: python addition.py <num1> <num2>")
         sys.exit(1)
 
-    x = int(sys.argv[1])
-    y = int(sys.argv[2])
+    try:
+        x = float(sys.argv[1])
+        y = float(sys.argv[2])
+    except ValueError:
+        print("Error: Inputs must be numbers.")
+        sys.exit(1)
 
-    print("Sum:", add(x, y))
+    print("Sum:", add(x, y))
+
+if __name__ == "__main__":
+    main()
