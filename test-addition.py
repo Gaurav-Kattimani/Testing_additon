be54ffa34c39f53682c807713a1672d70591c858
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-24 14:57:34 +0530

    Fix test for adding negative numbers

--- test-addition.py
+++ test-addition.py
@@ -4,7 +4,7 @@ def test_add_positive_numbers():
     assert add(2,3) == 5
 
 def test_add_negative_numbers():
-    assert add(-4,-6) == 5
+    assert add(-4,-6) == -10
 
 def test_add_zero_numbers():
     assert add(0,3) == 5
